6cb3d08bde35334c0686c942e4f0b09e0d6c7217
Tamzid Hossen <tamzidhossen366@gmail.com>
2026-01-14 18:25:02 +0600

    Codeforces Solution Log [Updated]

--- Src/solutions.cpp
+++ Src/solutions.cpp
@@ -1,5 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+#define FOR(i,x) for(ll i=0; i<x; i++)
+#define FORN(i,x) for(ll i=1; i<x; i++)
+
+#define tc() ll t; cin >> t; while(t--)
 void file()
 {
     #ifndef ONLINE_JUDGE
@@ -10,16 +17,21 @@ void file()
 }
 
 void Solutions(){
-    
+    int n; cin >> n;
+
+    if(n <= 1){
+        cout << "0\n";
+    } else {
+        cout << (n%2 !=0 ? 0 : n/4 + 1) << "\n";
+    }
 }
 
 int main()
 {
+    optimize();
     file();
-    int t; cin >> t;
-
-    while(t--){
+    tc(){
         Solutions();
-    }
+    };
     return 0;
 }
