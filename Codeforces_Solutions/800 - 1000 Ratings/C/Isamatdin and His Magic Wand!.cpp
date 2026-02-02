c85687dde6c2df0d127069244949071e8382e34e
Tamzid Hossen <tamzidhossen366@gmail.com>
2026-02-02 23:09:58 +0600

    Codeforces Solutions Log [ Updated ]

---
+++ Codeforces_Solutions/800 - 1000 Ratings/C/Isamatdin and His Magic Wand!.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+#define FOR(i,x) for(ll i=0; i<x; i++)
+#define FORN(i,x) for(ll i=1; i<=x; i++)
+
+#define tc() ll t; cin >> t; while(t--) Solutions()
+void file()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    // freopen("error.txt","w",stderr);
+    #endif
+}
+
+/**
+ * Problem Name: Isamatdin and His Magic Wand!
+ * Problem Link: https://codeforces.com/contest/2167/problem/C
+ * Difficulty: 800
+ * Topic: Constructive Algorithms, Greedy, Implementations, Sortings
+ */
+
+void Solutions(){
+    int n;  cin >> n;
+    vector<int> v(n);
+
+    int tmp1 = 0, tmp2 = 0;
+    for(auto &u: v){
+        cin >> u;
+        if(u % 2 == 0) tmp1++;
+        else tmp2++;
+    }
+
+    if(tmp1>0 && tmp2>0){
+        sort(v.begin(), v.end());
+    }
+
+    for(auto u: v){
+        cout << u << " ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    optimize();
+    file();
+    tc();
+    return 0;
+}
