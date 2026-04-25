dfa6c2392aa21fc5c8c3e65ced49367313917e0b
Tamzid Hossen <tamzidhossen366@gmail.com>
2026-04-25 18:51:15 +0600

    Algorithm Explaination [ swaping ]

---
+++ Src/Algorithm.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+#define FOR(i,x) for(ll i=0; i<x; i++)
+#define FORN(i,x) for(ll i=1; i<=x; i++)
+
+#define tc() ll t; cin >> t; while(t--) Solutions()
+void file()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    // freopen("error.txt","w",stderr);
+    #endif
+}
+
+void swap(int &a, int &b){  //& This is Reference
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void Solutions(){
+    int a = 5, b = 9;
+    swap(a, b);
+    cout << a << " " << b << endl;
+}
+
+int main()
+{
+    optimize();
+    file();
+    tc();
+    return 0;
+}
