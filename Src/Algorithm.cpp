7624ae57b95862f6970cb4799fb05dd18c25b742
Tamzid Hossen <tamzidhossen366@gmail.com>
2026-04-27 21:50:18 +0600

    Bubble Sort Algorithm Explain

--- Src/Algorithm.cpp
+++ Src/Algorithm.cpp
@@ -20,12 +20,44 @@ void swap(int &a, int &b){  //& This is Reference
     int tmp = a;
     a = b;
     b = tmp;
+
+    // int a = 5, b = 9;
+    // swap(a, b);
+    // cout << a << " " << b << endl;
+}
+
+void BubbleSort(int arr[], int n) {
+    bool check = false;
+    for(int i=0; i < n-1; i++){
+        for(int j=0; j<n-1-i; j++){
+            if(arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+                check = true;
+            }
+        }
+        if(!check){ cout << "off" << endl; break; }
+    }
+
+
+    // Solution Part
+    /*
+    int arr[] = { 54, 23, 34, 53, 32 };
+    // int arr[] = { 1, 2, 3, 4, 5 };
+    int n = 5;
+    cout << "Befor: ";
+    for(int i=0; i<n; i++){ cout << arr[i] << " "; }
+    cout << endl;
+
+    BubbleSort(arr, n);
+    cout << "After: ";
+    for(int j=0; j<n; j++){ cout << arr[j] << " "; }
+    cout << endl;
+    */
+
 }
 
 void Solutions(){
-    int a = 5, b = 9;
-    swap(a, b);
-    cout << a << " " << b << endl;
+    
 }
 
 int main()
