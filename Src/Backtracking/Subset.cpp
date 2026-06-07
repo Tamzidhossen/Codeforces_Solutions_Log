c94b96f4257578345dce9d736425de514a3d87a8
Tamzid Hossen <tamzidhossen366@gmail.com>
2026-06-07 19:17:44 +0600

    Added Backtracking Problem [Subset]

---
+++ Src/Backtracking/Subset.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+#define FOR(i,x) for(ll i=0; i<x; i++)
+#define FORN(i,x) for(ll i=1; i<=x; i++)
+
+#define tc() ll t; cin >> t; while(t--) Solutions()
+void file()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    // freopen("error.txt","w",stderr);
+    #endif
+}
+
+int subset(vector<int> &nums, vector<int> &ans, int idx){
+    //Base Case
+    if(idx == nums.size()){
+        cout << "{ ";
+        for(auto u: ans){
+            cout << u << " ";
+        }
+        cout << "}" << endl;
+        return 0;
+    }
+
+    ans.push_back(nums[idx]);
+    subset(nums, ans, idx+1);
+
+    //Backtrack
+    ans.pop_back();
+    subset(nums, ans, idx+1);
+}
+
+void Solutions(){
+    vector<int> nums={1,2,3};
+    vector<int> ans;
+
+    subset(nums, ans, 0);
+    //Ans: { }, { 1 }, { 1 2 }, { 1 2 3 },{ 1 3 }, { 2 }, { 2 3 }, { 3 }
+}
+
+int main()
+{
+    optimize();
+    file();
+    tc();
+    return 0;
+}
